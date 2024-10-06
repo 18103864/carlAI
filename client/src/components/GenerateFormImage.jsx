7751ba54a731aba8889555d1da7dc367c0ec56e0
Carl <18103864@usc.edu.ph>
2024-10-06 18:52:08 +0800

    completed front end

---
+++ client/src/components/GenerateFormImage.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import styled from 'styled-components'
+import Button from './buttons';
+import TextInput from './TextInput';
+import { AutoAwesome, CreateRounded } from '@mui/icons-material';
+
+const Form = styled.div`
+  flex: 1;
+  padding: 16px 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 9%;
+  justify-content: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+  align-items: center;
+`;
+
+const Title = styled.div`
+  font-size: 28px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+`;
+
+const Desc = styled.div`
+  font-size: 17px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+
+const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+  font-size: 12px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.text_secondary};
+`;
+
+const Actions = styled.div`
+  flex: 1;
+  display: flex;
+  gap: 8px;
+`;
+
+const GenerateFormImage = ({
+    post,
+    setPost,
+    createPostLoading,
+    generateImageLoading,
+    setCreatePostLoading,
+    setGenerateImageLoading,
+}) => {
+
+    const generateImageFunc = () => {
+        setGenerateImageLoading(true);
+    };
+
+    const createPostFunc = () => {
+        setCreatePostLoading(true);
+    };   
+
+  return (
+    <Form>
+        <Top>
+            <Title>Generate Image with Prompt</Title>
+            <Desc>Write Your Prompt</Desc>
+        </Top>
+        <Body>
+            <TextInput 
+                label="Author" 
+                placeholder="Enter your name" 
+                name="name"
+                value={post.name}
+                handelChange={(e)=>setPost({...post, name: e.target.value})}
+            />
+            <TextInput 
+                label="Image Prompt" 
+                placeholder="Write your image prompt..." 
+                name="name"
+                rows="8"
+                textArea
+                value={post.prompt}
+                handelChange={(e)=>setPost({...post, prompt: e.target.value})}
+            />
+            ** You can post the AI generated Image to the Community
+        </Body>
+        <Actions>
+            <Button 
+                text="Generate Image" 
+                flex 
+                leftIcon={<AutoAwesome/>}
+                isLoading={generateImageLoading}
+                isDisabled={post.prompt === ""}
+                onClick={()=>generateImageFunc()}
+            />
+            <Button 
+                text="Post Image" 
+                flex 
+                type="secondary" 
+                leftIcon={<CreateRounded/>}
+                isLoading={createPostLoading}
+                isDisabled={post.name === "" || post.prompt === "" || post.photo === ""}
+                onClick={()=>createPostFunc()}
+            />
+        </Actions>
+    </Form>
+  )
+}
+
+export default GenerateFormImage
--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,14 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import GenerateFormImage from '../components/GenerateFormImage';
+import GeneratedImageCard from '../components/GeneratedImageCard';
 
 const Container = styled.div`
   height:100%;
   overflow-y: scroll;
+  background: ${({theme}) => theme.bg}
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  @media (max-width: 768px){
+    padding: 6px 10px;
+  }
 `;
 
+const Wrapper = styled.div`
+  
+  height: fit-content;
+  width: 100%;
+  max-width: 1200px;
+  gap: 8%;
+  display: flex;
+  justify-content: center;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+
 const CreatePost = () => {
+  const [generateImageLoading, setGenerateImageLoading] = useState(false);
+  const [createPostLoading, setCreatePostLoading] = useState(false);
+  const [post, setPost] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  })
   return (
-    <Container>CreatePost</Container>
+    <Container>
+      <Wrapper>
+        <GenerateFormImage 
+        post={post} 
+        setPost={setPost} 
+        createPostLoading={createPostLoading}
+        generateImageLoading={generateImageLoading}
+        setCreatePostLoading={setCreatePostLoading}
+        setGenerateImageLoading={setGenerateImageLoading}
+        />
+        <GeneratedImageCard src={post?.photo} loading={generateImageLoading}/>
+      </Wrapper>
+    </Container>
   )
 }
 
